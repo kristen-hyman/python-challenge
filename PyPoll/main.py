be20e12f3c69bd8d2ad4e534142d1a69b8db3f8c
Kristen Hyman <kristen.hyman@pwcusgov.com>
2018-09-15 12:43:43 -0400

    python hw update

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,6 +6,7 @@ hwfile = os.path.join("election_data.csv")
 
 totalvotes = 0 
 CandidateList = []
+KhanList = []
 
 with open(hwfile, newline="") as ElectionData:
     csvreader = csv.reader(ElectionData, delimiter=",")
@@ -20,11 +21,13 @@ with open(hwfile, newline="") as ElectionData:
         
         if row[2] not in CandidateList:
             CandidateList.append(row[2])
+            print(CandidateList)
             
-print(CandidateList)
-
 #The percentage of votes each candidate won
-
+#create a list of only votes that relate to a certain candidate
+        if row[2] == "Khan":
+            KhanList.append(row[0])
+        print(KhanList)
 
 #The total number of votes each candidate won
 
